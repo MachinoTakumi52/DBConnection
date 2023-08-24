10b2d347fa41d65af28fbab41992a564aa1ce1c0
machino_takumi <takumi.machino@vss.co.jp>
2023-08-24 22:02:13 +0900

    ポスグレ用ソリューション追加

---
+++ DBConnection/DBConnectionForPostgreSQL/DataBaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using DBConnectionTools.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnectionForPostgreSQL
+{
+    class DataBaseConnection : AbstractDBConnection
+    {
+    }
+}
